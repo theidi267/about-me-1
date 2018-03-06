e3b1acb2d5f9fb5e566efd091024e44b21bd6128
Jason Burns <jasonb315@protonmail.com>
2018-03-06 13:49:55 -0800

    This is a Git commit to verify Git function

---
+++ js/app.js
@@ -0,0 +1,31 @@
+'use strict' //code checker, links to ".eslintrc.json" in webpage emigen folder
+
+///
+var petQuestion = parseInt(prompt('How many pets do you think I have? please enter a number'));
+
+if(petQuestion === 2 ) {
+    alert('correct');
+
+}
+else{
+    alert('nope');
+}
+
+///
+var myAge = prompt('Do you think I\'m older than 30? Please enter yes or no')
+
+if(myAge.toLowerCase() ==='yes'){
+    alert('That is correct')
+}
+else{'No, I am older than 30!'}
+
+
+// if(first condition);{
+//     do this;
+// }
+// else if(second contition){
+//     do this;
+// }
+// else{
+//     do this;
+// }
