ff7cf767d687ebd41c2163cdea1079c3173dbc44
Jason Burns <jasonb315@protonmail.com>
2018-03-06 18:05:19 -0800

    header updated, quiz refined, counter repaired

--- js/app.js
+++ js/app.js
@@ -1,48 +1,60 @@
 'use strict'
 document.getElementById('pic2').onclick=function(){
 
+var counter = 0
+
+alert("Game: answer with \'yes\' or \'no\'");
+
 var questionOne = prompt('Would you guess that I\'m from Seattle?');
     if(questionOne.toLowerCase() === 'yes'){
         alert('That\'s right!');
-        console.log('+1 correct answer') 
+        console.log('+1 correct answer');
+        counter++;
     } else {
-        alert('Ah you think rain is your ally? You merely adopted the rain. I was born in it, molded by it.');
-        console.log('+0 correct answer') 
+        alert('Wrong! You think rain is your ally? You merely adopted the rain. I was born in it, molded by it.');
+        console.log('+0 correct answer'); 
     };
    
-var questionTwo = prompt('Am I older than 30?');
+var questionTwo = prompt('Would you guess that I\'m 28?');
     if(questionTwo.toLowerCase() === 'yes'){
-        alert('wrong..');
-        console.log('+0 correct answer') 
+        alert('correct!');
+        console.log('+1 correct answer');
+        counter++;
     } else {
-        alert('That\s right! I\'m 28 until March 15');
-        console.log('+1 correct answer') 
+        alert('I am in fact 28 years old.');
+        console.log('+0 correct answer');
+        
     };
 
-var questionThree = prompt('Would you say I\'m white?');
+var questionThree = prompt('Do you think I would say that I\'m white?');
     if(questionThree.toLowerCase() === 'yes'){
-        alert('Nah, just a programmer');
-        console.log('+0 correct answer') 
+        alert('Nah, programmer is my race.');
+        console.log('+0 correct answer'); 
     } else {
-        alert('That\'s right, I\'m just a programmer.');
-        console.log('+1 correct answer') 
+        alert('That\'s right, my race is programmer.');
+        console.log('+1 correct answer');
+        counter++;
     };
 
 var questionFour = prompt('Are all my tattoos red?');
     if(questionFour.toLowerCase() === 'yes'){
         alert('ding ding ding!');
-        console.log('+1 correct answer') 
+        console.log('+1 correct answer');
+        counter++; 
     } else {
-        alert('BZZZZZZZ');
-        console.log('+0 correct answer') 
+        alert('BZZZZZZZ, they are all red.');
+        console.log('+0 correct answer'); 
     };
 
-var questionFive = prompt('want to be friends?');
+var questionFive = prompt('Wanna be friends?');
     if(questionFive.toLowerCase() === 'yes'){
         alert('Hit me up on Slack!');
-        console.log('+1 correct answer') 
+        console.log('+1 correct answer');
+        counter++; 
     } else {
         alert('Psht. Whatever.');
-        console.log('+0 correct answer') 
+        console.log('+0 correct answer'); 
     };    
+
+    console.log('correct answers: ' + counter + '/5');
 };
