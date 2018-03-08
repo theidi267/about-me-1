6fb7ed4c75228956a1408c8be56509de594f04ad
Jason Burns <jasonb315@protonmail.com>
2018-03-08 13:31:29 -0800

    Q:1-5 given response for <1 character entry.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # about-me
 About me project for week 1 of Code Fellows 201.
+
+STUDENT: .md extensions are like this: https://en.wikipedia.org/wiki/Markdown
+
--- js/app.js
+++ js/app.js
@@ -3,13 +3,20 @@
 
 var counter = 0
 
+//introduction
 var userName = prompt("Hey! I\'m Jason. I made this page about myself. Who are you?");
 alert('Nice to meet you ' + userName + '. ');
-
 alert('Ok, so I\'ve made a yes or no guessing game out of these prompts and alerts, cool right?');
 alert('If you get a question right, you get a point. If you get it wrong, ya don\'t! yes or no:');
+
 //Question 1
 var questionOne = prompt('Would you guess that I\'m from Seattle?');
+
+    while (questionOne.length <1){
+        alert('you did not answer');
+        questionOne = prompt('Would you guess that I\'m from Seattle?');
+    };
+
     if(questionOne.toLowerCase() === 'yes'){
         alert('It\'s true, I am.');
         console.log('+1');
@@ -21,6 +28,17 @@ var questionOne = prompt('Would you guess that I\'m from Seattle?');
 
 //Question 2
 var questionTwo = prompt('Would you guess that I\'m 28?');
+
+    while (questionTwo.length <1){
+        alert('you did not answer');
+        questionTwo = prompt('Would you guess that I\'m 28?');
+    };
+
+    while (questionTwo.length <1){
+    alert('you did not answer');
+    ageGuess = prompt ('question about age');
+    };
+    
     if(questionTwo.toLowerCase() === 'yes'){
         alert('Yes? You would be right!');
         console.log('+1');
@@ -33,6 +51,12 @@ var questionTwo = prompt('Would you guess that I\'m 28?');
 
 //Question 3
 var questionThree = prompt('Am I programmer?');
+
+    while (questionThree.length <1){
+        alert('you did not answer');
+        questionThree = prompt('Am I programmer?');
+    };
+
     if(questionThree.toLowerCase() === 'yes'){
         alert('Yes It\'s true! I programmed this!');
         console.log('+1'); 
@@ -44,6 +68,12 @@ var questionThree = prompt('Am I programmer?');
 
 //Question 4
 var questionFour = prompt('Would you suppose I\'m dog friendly?');
+
+    while (questionFour.length <1){
+        alert('you did not answer');
+        questionFour = prompt('Would you suppose I\'m dog friendly?');
+    };
+
     if(questionFour.toLowerCase() === 'yes'){
         alert('It\'s true I love dogs. More than people sometimes. Usually.');
         console.log('+1');
@@ -55,6 +85,12 @@ var questionFour = prompt('Would you suppose I\'m dog friendly?');
 
 //Question 5
 var questionFive = prompt('Wanna be friends?');
+
+    while (questionFive.length <1){
+        alert('you did not answer');
+        questionFive = prompt('Wanna be friends?');
+    };
+
     if(questionFive.toLowerCase() === 'yes'){
         alert('You should hit me up on Slack');
         console.log('+1');
@@ -68,6 +104,7 @@ var questionFive = prompt('Wanna be friends?');
 for(var sixLoop = 0; sixLoop < 4; sixLoop++) {
 
 var questionSix = parseInt(prompt('You get four guesses, what is my favorite number? HINT: it\'s three digits.'));
+    
     if(questionSix > 315){
         alert('Lower, guess again.');
         
@@ -80,15 +117,17 @@ var questionSix = parseInt(prompt('You get four guesses, what is my favorite num
         break;
     };
 };
+
 //Question 7
 var counterState = 0;
 var correct = false;
 var myStates = ['washington', 'florida', 'south Carolina', 'alaska'];
 
-while (correct === false && counterState < 6){
+//TODO counterState < 24 yeilds 6 guesses. why? magic.. [ the loop in loop has something to do with this? ]
+while (correct === false && counterState < 24){
     var theirGuess = (prompt('Can you guess a state that I have lived in besides Washington?')).toLowerCase();
     for (var i = 0; i < myStates.length; i++) {
-        if (myStates[i] === theirGuess) {
+        if (myStates[i] === theirGuess && theirGuess !=null) { //Attention to this null, working?
             console.log(i);
             alert('Nice Guess!');
             console.log(counterState + ' attempts made');
@@ -107,12 +146,7 @@ if(correct === false ) {
     alert('Florida would have been a good guess, but there\'s no place like home.');
        }
         
-    
-
-
-
-    alert('Thanks for playing! ' + userName + ' you got ' + counter + '/6 correct!');
-    console.log('correct answers: ' + counter + '/6');
-
+    alert('Thanks for playing! ' + userName + ' you got ' + counter + '/7 correct!');
+    console.log('correct answers: ' + counter + '/7');
 
 // };
